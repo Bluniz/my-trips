138b91755077fc8265b32f31216cc829e399471d
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-10-09 19:42:23 -0300

    Remove storybook and fix some things

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,15 +1,1 @@
-import * as S from './styles'
-
-export const Main = () => (
-  <S.Wrapper>
-    <S.Logo src="img/logo.svg" alt="React Avançado" />
-    <S.Title>React Avançado</S.Title>
-    <S.Description>
-      Typescript, ReactJS, NextJS e Styled Components
-    </S.Description>
-    <S.Illustration
-      src="img/hero-illustration.svg"
-      alt="Um desenvolvedor de frete para tela com código"
-    />
-  </S.Wrapper>
-)
+export const Main = () => <h1>NextJS na prática</h1>
