c09e23fa0a868b50a79135af815b4274be362d1e
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-10-19 08:56:37 -0300

    fix analytics again

--- src/components/Analytics/index.tsx
+++ src/components/Analytics/index.tsx
@@ -3,18 +3,18 @@ export const Analytics = () => (
     <script
       async
       src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING}`}
-    ></script>
-
+    />
     <script
       dangerouslySetInnerHTML={{
-        __html: `window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js' new Date());
-          gtag('config','${process.env.NEXT_PUBLIC_GA_TRACKING}', {
-            page_path: window.location.pathname,
-          });
+        __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING}', {
+              page_path: window.location.pathname,
+            });
           `
       }}
-    ></script>
+    />
   </>
 )
